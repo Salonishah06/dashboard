ee8d02860db9d1f2b4a9f2cdbc49832c44adacb1
Saloni <salonishah306@gmail.com>
2022-11-20 09:05:45 +0530

    Single barchart implemented

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,86 +1,60 @@
-// import React from 'react';
-// import {Bar} from 'react-chartjs-2';
+import React, { Component } from 'react';
+import { Bar,Line,Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, registerables } from 'chart.js';
+import { Chart } from 'react-chartjs-2'
+ChartJS.register(...registerables);
 
-// const BarChart = () =>{
-//     return<div>
-//         <Bar
-//         data={{
-//             labels:['Red','Blue','Yellow','Green','Purple','Orange'],
-//         }}
-//         height={400}
-//         width={200}
-//         options={{
-//             maintainAspectRatio:false,
-//         }}
-//         />
-//     </div>
-// }
 
-// export default BarChart;
-import React from 'react'
-import { Pie, defaults } from 'react-chartjs-2'
+class BarChart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData: {
+        labels: [
+          'Boston',
+          'Worcester',
+          'Springfield',
+          'Lowell',
+          'Cambridge',
+          'New Bedford',
+        ],
+        datasets: [
+          {
+            label: 'Population',
+            data: [
+                  617594,
+                  181045, 
+                  153060,
+                  106519, 
+                  105162, 
+                  95072
+                ],
+                 backgroundColor: [
+                 'rgba(255,99,132,0.6)',
+                  'rgba(54,162,235,0.6)',
+                  'rgba(255,206,86,0.6)',
+                 'rgba(75,192,192,0.6)',
+              'rgba(153,102,255,0.6)',
+              'rgba(255,159,64,0.6)',
+              'rgba(255,99,132,0.6)'
+            ]
+          }
+        ]
+      }
+    }
+  }
 
-defaults.global.tooltips.enabled = false
-defaults.global.legend.position = 'bottom'
-
-const BarChart = () => {
-  return (
-    <div>
-      <Pie
-        data={{
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [
-            {
-              label: '# of votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-              ],
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-              ],
-              borderWidth: 1,
-            },
-            // {
-            //   label: 'Quantity',
-            //   data: [47, 52, 67, 58, 9, 50],
-            //   backgroundColor: 'orange',
-            //   borderColor: 'red',
-            // },
-          ],
-        }}
-        height={400}
-        width={600}
-        options={{
-          maintainAspectRatio: false,
-          scales: {
-            yAxes: [
-              {
-                ticks: {
-                  beginAtZero: true,
-                },
-              },
-            ],
-          },
-          legend: {
-            labels: {
-              fontSize: 25,
-            },
-          },
-        }}
-      />
-    </div>
-  )
+  render() {
+    return (
+      <div className="BarChart">
+        <Bar
+          data={this.state.chartData}
+          options={{
+          
+          }}
+        />
+      </div>
+    )
+  }
 }
-
 export default BarChart;
